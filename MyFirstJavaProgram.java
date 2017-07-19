4aedc9fa9c386e23be004032411b9d355c38a345
fredthefrenchtoast <30299526+fredthefrenchtoast@users.noreply.github.com>
2017-07-19 11:56:26 -0700

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,25 @@
+package day1.robot;
+
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		
+		// START HERE
+	Robot fred = new Robot();
+	fred.penDown();
+		
+	
+	for (int i = 0; i < 3; i++) {
+		
+	fred.turn(120);
+	fred.move(100);
+	int sides = 5;
+	}
+	
+	
+	
+	
+	}
+}
---
+++ Spiral.java
@@ -0,0 +1,28 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Spiral {
+
+	public static void main(String[] args) {
+		Robot john = new Robot();
+		
+		// 5. Set your robot's pen to the down position
+john.penDown();
+		// 3. Set the robot to go at max speed (10)
+john.setSpeed(10);
+		// 4. Do the following (steps 6-9) 75 times
+for (int i = 0; i < 100000; i++) {
+	
+
+			// 7. Change the pen color to random
+	john.setRandomPenColor();
+			// 6. Move the robot 5 times the current line number you are drawing (5*i)
+	john.move( i/3300);
+			// 2. Turn the robot 1/3 of 360 degrees to the right
+	john.turn(360/330);
+			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
+			// 9. Set the pen width to i
+john.setPenWidth(i);}
+	}
+}
